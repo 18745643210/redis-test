44b0772f8907fe6b4baa6f2f4e7c79352f1d426a
cuiyang <31043356@qq.com>
2020-08-20 19:34:39 +0800

    初次提交

--- src/main/java/com/cuiyang/redistest/RedisTestApplication.java
+++ src/main/java/com/cuiyang/redistest/RedisTestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisTestApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(RedisTestApplication.class, args);
     }
 
