89947aba5cddc5a804dc7931845b95cd8dcb9f6d
cuiyang <31043356@qq.com>
2020-08-21 10:36:46 +0800

    "测试"

--- src/main/java/com/cuiyang/redistest/service/TestService.java
+++ src/main/java/com/cuiyang/redistest/service/TestService.java
@@ -7,4 +7,28 @@ public class TestService {
     public String getName() {
         return "cuiyang";
     }
+
+    public int[] merge(int a[], int m, int b[], int n) {
+        int c[] = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < m && j < n) {
+            if (a[i] <= b[j]) {
+                c[k++] = a[i];
+            } else {
+                c[k++] = b[j];
+            }
+        }
+        while (i < m) {
+            c[k++] = a[i++];
+        }
+        while (j < n) {
+            c[k++] = b[j++];
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+    }
 }
